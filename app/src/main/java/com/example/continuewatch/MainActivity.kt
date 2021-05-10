9568279fb3d66f7dcb4fdc4e1d29b0afc3bb4a71
Violetta Mamykina <violetta-1998m@mail.ru>
2021-05-10 17:19:09 +0300

    check wrong program

--- app/src/main/java/com/example/continuewatch/MainActivity.kt
+++ app/src/main/java/com/example/continuewatch/MainActivity.kt
@@ -6,7 +6,6 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     var secondsElapsed: Int = 0
-
     var backgroundThread = Thread {
         while (true) {
             Thread.sleep(1000)
@@ -15,7 +14,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
